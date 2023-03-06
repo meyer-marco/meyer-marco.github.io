b973e119a597e6ba23c2769a57bc06fc570ac015
Marco Meyer <marcomeyer@posteo.de>
2023-03-06 14:08:49 +0100

    init conf

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://meyer-marco.github.io/hammerstuecke.github.io/",
-  base: "/hammerstuecke.github.io",
+  site: "https://meyer-marco.github.io/",
 });
